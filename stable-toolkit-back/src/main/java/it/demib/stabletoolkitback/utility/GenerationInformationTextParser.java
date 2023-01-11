644127d6a89d63a9638fda6835d663dc9d0cfc71
demibit <keveszm@gmail.com>
2023-01-11 09:52:20 +0000

    Made generation parameter parsing more efficient and readable

--- stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/utility/GenerationInformationTextParser.java
+++ stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/utility/GenerationInformationTextParser.java
@@ -13,28 +13,11 @@ public class GenerationInformationTextParser implements
   public Map<String, String> parse(List<String> textData) {
     Map<String, String> info = new HashMap<>();
 
-    String[] rawInfo = (textData.get(2) + ",").split(" ");
+    String[] rawInfo = textData.get(2).split(",");
 
-    for (int i = 0; i < rawInfo.length; i++) {
-      if (rawInfo[i].contains(",")) {
-        StringBuilder genTag = new StringBuilder();
-        StringBuilder genVal = new StringBuilder(
-            rawInfo[i].replaceAll(":", "").replaceAll(",", "") + " ");
-
-        boolean atTag = false;
-
-        for (int j = i - 1; j >= 0 && !rawInfo[j].contains(","); j--) {
-          if (rawInfo[j].contains(":") || atTag) {
-            atTag = true;
-            genTag.insert(0,
-                rawInfo[j].replaceAll(":", "").replaceAll(",", "") + " ");
-          } else {
-            genVal.insert(0,
-                rawInfo[j].replaceAll(":", "").replaceAll(",", "") + " ");
-          }
-        }
-        info.put(genTag.toString().trim(), genVal.toString().trim());
-      }
+    for (String parameters : rawInfo) {
+      String[] splitParameters = parameters.split(":");
+      info.put(splitParameters[0].trim(), splitParameters[1].trim());
     }
 
     info.put("Positive prompt", textData.get(0));
