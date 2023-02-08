7f42f32b48fb45a36efb684e73ae776236e3d7ae
demibit <keveszm@gmail.com>
2023-02-08 19:57:23 +0000

    Add logging to SettingService

--- stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/SettingService.java
+++ stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/SettingService.java
@@ -2,11 +2,15 @@ package it.demib.stabletoolkitback.service;
 
 import it.demib.stabletoolkitback.model.entity.Setting;
 import it.demib.stabletoolkitback.repository.SettingRepository;
+import it.demib.stabletoolkitback.utility.LogMessageUtility;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class SettingService {
@@ -23,8 +27,17 @@ public class SettingService {
 
   public Boolean deleteSettings(List<Setting> settings) {
     if (settings.size() > 0) {
+      log.info(LogMessageUtility.assembleLogMessage("SettingService", "deleteSettings",
+          String.format("Deleting settings from database: %s",
+              settings.stream()
+                  .map(Setting::getName)
+                  .collect(Collectors.joining(", ")))));
+
       settingRepository.deleteAll(settings);
     } else {
+      log.info(LogMessageUtility.assembleLogMessage("SettingService", "deleteSettings",
+          "Deleting all settings from database"));
+
       settingRepository.deleteAll();
     }
 
