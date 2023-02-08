d3ec534f93ca4bc35658e2dfeaa95e295052ad1d
demibit <keveszm@gmail.com>
2023-02-08 19:51:50 +0000

    Add logging to FolderService

--- stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/FolderService.java
+++ stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/FolderService.java
@@ -2,6 +2,7 @@ package it.demib.stabletoolkitback.service;
 
 import it.demib.stabletoolkitback.model.entity.Folder;
 import it.demib.stabletoolkitback.repository.FolderRepository;
+import it.demib.stabletoolkitback.utility.LogMessageUtility;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -9,8 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
 @Slf4j
+@Service
 @AllArgsConstructor(onConstructor_ = @Autowired)
 public class FolderService {
 
@@ -26,9 +27,17 @@ public class FolderService {
 
   protected void deleteFolders(List<Folder> folders) {
     if (folders.size() > 0) {
+      log.info(LogMessageUtility.assembleLogMessage("FolderService", "deleteFolders",
+          String.format("Deleting folders from database: %s",
+              folders.stream()
+                  .map(Folder::getName)
+                  .collect(Collectors.joining(", ")))));
+
       folderRepository.deleteAllById(
           folders.stream().map(Folder::get_id).collect(Collectors.toList()));
     } else {
+      log.info(LogMessageUtility.assembleLogMessage("FolderService", "deleteFolders", "Deleting all folders from database"));
+
       folderRepository.deleteAll();
     }
   }
