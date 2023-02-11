5a3a0383fd1138c648e47feddff083b0499f52b0
demibit <keveszm@gmail.com>
2023-02-11 12:15:05 +0000

    Add test db

--- stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/configuration/MongoDbConfiguration.java
+++ stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/configuration/MongoDbConfiguration.java
@@ -2,25 +2,28 @@ package it.demib.stabletoolkitback.configuration;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@AllArgsConstructor(onConstructor_ = @Autowired)
+@RequiredArgsConstructor
 public class MongoDbConfiguration {
 
   private final MongoClient mongoClient;
 
+  @Value("${spring.data.mongodb.database}")
+  private String databaseName;
+
   @Bean("IMAGE")
   public MongoCollection<Document> getImageCollection() {
-    return mongoClient.getDatabase("stable-toolkit").getCollection("image");
+    return mongoClient.getDatabase(databaseName).getCollection("image");
   }
 
   @Bean("TAG")
   public MongoCollection<Document> getTagCollection() {
-    return mongoClient.getDatabase("stable-toolkit").getCollection("tag");
+    return mongoClient.getDatabase(databaseName).getCollection("tag");
   }
 }
