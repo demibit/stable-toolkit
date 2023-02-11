c1c70eea20cab9b3fdef8e85311234c51389456c
demibit <keveszm@gmail.com>
2023-02-11 14:19:41 +0000

    Improve logging in IndexService, and separating image indexing to its own method

--- stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/IndexService.java
+++ stable-toolkit-back/src/main/java/it/demib/stabletoolkitback/service/IndexService.java
@@ -61,9 +61,6 @@ public class IndexService {
   }
 
   private void indexFolder(Folder folder) {
-    log.info(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-        String.format("Indexing folder: %s", folder.getName())));
-
     List<Image> images = imageService.findAll();
     Set<String> imageFileNameSet = images.stream()
         .map(Image::getFileName)
@@ -74,93 +71,21 @@ public class IndexService {
     long sizeBefore = imageService.count();
 
     try (Stream<Path> paths = Files.walk(Paths.get(folder.getPath()))) {
-      paths
-          .filter(Files::isRegularFile)
-          .filter(path -> images.stream()
-              .noneMatch(image -> path.getFileName().toString().equals(image.getFileName())))
-          .parallel()
-          .forEach(path -> {
-            String currentFileName = path.getFileName().toString();
+      List<Path> pathsToIndex = paths
+          .filter(path -> Files.isRegularFile(path) && !imageFileNameSet.contains(
+              path.getFileName().toString()) && path.getFileName().toString()
+              .endsWith(ImageType.PNG))
+          .toList();
 
-            if (currentFileName.endsWith(ImageType.PNG) && !imageFileNameSet.contains(
-                currentFileName)) {
-              log.info(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-                  String.format("Indexing image %s", currentFileName)));
+      if (!pathsToIndex.isEmpty()) {
+        log.info(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
+            String.format("Indexing folder for new images: %s", folder.getName())));
 
-              List<String> tagsToAssign = new ArrayList<>();
-              Map<String, String> info = new HashMap<>();
-              List<String> textData = new ArrayList<>();
-              BufferedImage bimg = null;
+        pathsToIndex.stream()
+            .parallel()
+            .forEach(path -> indexImage(path, currentTags, folder));
+      }
 
-              try {
-                File img = new File(path.toUri());
-                bimg = ImageIO.read(img);
-                textData = Arrays.asList(
-                    pngTextExtractor.showText(new FileInputStream(img)));
-              } catch (Exception e) {
-                log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-                    String.format("Unable to read the image chunks of %s, continuing without it",
-                        currentFileName)));
-              }
-
-              if (textData.size() >= 3) {
-                log.info(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-                    String.format("Attempting to parse the generation text of %s",
-                        currentFileName)));
-
-                tagsToAssign = tagParser.parse(Pair.of(currentTags, textData.get(0)));
-                info = generationInformationTextParser.parse(textData);
-              }
-
-              Instant creationDate = null;
-              Integer width = null;
-              Integer height = null;
-              try {
-                creationDate = Files.readAttributes(path, BasicFileAttributes.class).creationTime()
-                    .toInstant();
-              } catch (Exception e) {
-                log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-                    String.format("Unable to read the creation date of %s, continuing without it",
-                        currentFileName)));
-              }
-
-              try {
-                Objects.requireNonNull(bimg);
-                width = bimg.getWidth();
-                height = bimg.getHeight();
-              } catch (Exception e) {
-                log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
-                    String.format(
-                        "Unable to read the image dimensions of %s, continuing without it",
-                        currentFileName)));
-              }
-
-              imageService.save(Image.builder()
-                  .location(folder.getPath())
-                  .fileName(path.getFileName().toString())
-                  .creationDate(creationDate)
-                  .tags(tagsToAssign)
-                  .positivePrompt(info.get("Positive prompt"))
-                  .negativePrompt(info.get("Negative prompt"))
-                  .steps(Objects.nonNull(info.get("Steps")) ? Integer.valueOf(info.get("Steps"))
-                      : null)
-                  .sampler(info.get("Sampler"))
-                  .denoise(Objects.nonNull(info.get("Denoising strength")) ? Double.valueOf(
-                      info.get("Denoising strength")) : null)
-                  .cfg(
-                      Objects.nonNull(info.get("CFG scale")) ? Double.valueOf(info.get("CFG scale"))
-                          : null)
-                  .modelHash(info.get("Model hash"))
-                  .modelName(info.get("Model"))
-                  .faceRestoration(info.get("Face restoration"))
-                  .hypernet(info.get("Hypernet"))
-                  .clipSkip(Objects.nonNull(info.get("Clip skip")) ? Integer.valueOf(
-                      info.get("Clip skip")) : null)
-                  .width(width)
-                  .height(height)
-                  .build());
-            }
-          });
     } catch (Exception e) {
       log.error(LogMessageUtility.assembleLogMessage("IndexService", "indexFolder",
           "Unable to index images"));
@@ -176,5 +101,84 @@ public class IndexService {
           String.format("Finished indexing folder: %s | total image count: %s", folder.getName(),
               sizeAfter)));
     }
+  }
+
+  private void indexImage(Path path, List<String> currentTags, Folder folder) {
+    String currentFileName = path.getFileName().toString();
+
+    log.info(LogMessageUtility.assembleLogMessage("IndexService", "indexImage",
+        String.format("Indexing image %s", currentFileName)));
+
+    List<String> tagsToAssign = new ArrayList<>();
+    Map<String, String> info = new HashMap<>();
+    List<String> textData = new ArrayList<>();
+    BufferedImage bimg = null;
+
+    try {
+      File img = new File(path.toUri());
+      bimg = ImageIO.read(img);
+      textData = Arrays.asList(
+          pngTextExtractor.showText(new FileInputStream(img)));
+    } catch (Exception e) {
+      log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexImage",
+          String.format("Unable to read the image chunks of %s, continuing without it",
+              currentFileName)));
+    }
+
+    if (textData.size() >= 3) {
+      tagsToAssign = tagParser.parse(Pair.of(currentTags, textData.get(0)));
+      info = generationInformationTextParser.parse(textData);
+    }
+
+    Instant creationDate = null;
+    Integer width = null;
+    Integer height = null;
+    try {
+      creationDate = Files.readAttributes(path, BasicFileAttributes.class)
+          .creationTime()
+          .toInstant();
+    } catch (Exception e) {
+      log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexImage",
+          String.format("Unable to read the creation date of %s, continuing without it",
+              currentFileName)));
+    }
+
+    try {
+      Objects.requireNonNull(bimg);
+      width = bimg.getWidth();
+      height = bimg.getHeight();
+    } catch (Exception e) {
+      log.warn(LogMessageUtility.assembleLogMessage("IndexService", "indexImage",
+          String.format(
+              "Unable to read the image dimensions of %s, continuing without it",
+              currentFileName)));
+    }
+
+    imageService.save(Image.builder()
+        .location(folder.getPath())
+        .fileName(path.getFileName().toString())
+        .creationDate(creationDate)
+        .tags(tagsToAssign)
+        .positivePrompt(info.get("Positive prompt"))
+        .negativePrompt(info.get("Negative prompt"))
+        .steps(Objects.nonNull(info.get("Steps")) ? Integer.valueOf(info.get("Steps"))
+            : null)
+        .sampler(info.get("Sampler"))
+        .denoise(Objects.nonNull(info.get("Denoising strength")) ? Double.valueOf(
+            info.get("Denoising strength")) : null)
+        .cfg(
+            Objects.nonNull(info.get("CFG scale")) ? Double.valueOf(
+                info.get("CFG scale"))
+                : null)
+        .modelHash(info.get("Model hash"))
+        .modelName(info.get("Model"))
+        .faceRestoration(info.get("Face restoration"))
+        .hypernet(info.get("Hypernet"))
+        .clipSkip(Objects.nonNull(info.get("Clip skip")) ? Integer.valueOf(
+            info.get("Clip skip")) : null)
+        .width(width)
+        .height(height)
+        .build());
+
   }
 }
